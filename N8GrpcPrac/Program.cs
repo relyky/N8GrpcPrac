91d334a4c39d75c343cf5d4638ab17534cb0cea0
Rely Ky <relyky@hotmail.com>
2025-06-18 16:00:01 +0800

    gRPC hello

--- N8GrpcPrac/Program.cs
+++ N8GrpcPrac/Program.cs
@@ -11,4 +11,7 @@ var app = builder.Build();
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
+// ¤½¥¬ proto ¤å¥ó
+app.MapGet("/protos/greet.proto", async context => await context.Response.SendFileAsync("Protos/greet.proto"));
+
 app.Run();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # N8GrpcPrac
 NET8 gRPC parctice
+
+# 參考文件
+ * [Docker for Beginners with Blazor](https://www.youtube.com/watch?v=Q_7rrpgy4Lo)
+  
