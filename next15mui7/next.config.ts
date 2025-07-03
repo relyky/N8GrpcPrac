5a95b84a5bb954d220e00902f20ffefeeaaec433
Rely Ky <relyky@hotmail.com>
2025-07-03 19:06:00 +0800

    nextConfig 在開發模式下允許跨源網址。

--- next15mui7/next.config.ts
+++ next15mui7/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.*.*'] // 在開發模式下允許跨源網址。
 };
 
 export default nextConfig;
